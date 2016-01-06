f5cec6273dc4cdd02d77283aa66505985d479ccd
Delyssa <daemonedge@mail.ru>
2016-01-06 13:02:20 +0300

    add dev seeds and comment factory

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,1 +1,8 @@
 FactoryGirl.create(:user, :confirmed, email: "user@example.com")
+10.times do |id|
+  FactoryGirl.create(:user, :confirmed, email: "user#{id + 1}@example.com")
+  FactoryGirl.create(:article, user: User.find(id + 1))
+end
+100.times do |comment_id|
+  FactoryGirl.create(:comment, user: User.find(comment_id / 10 + 1), article: Article.find(comment_id % 10 + 1))
+end
